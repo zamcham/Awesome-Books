8749abdb73f15e58f2e87f74eb87d7089fdadb2f
Julio Quezada <julio.quezada@catolica.edu.sv>
2023-03-21 10:41:18 -0600

    Add the render for UI

--- index.js
+++ index.js
@@ -39,3 +39,30 @@ class BooksListUI {
     this.booksContainer = document.querySelector('.booksContainer');
   }
 
+  render() {
+    this.booksContainer.innerHTML = '';
+    this.bookList.renderBooks((book, removeBookFn) => {
+      const bookInstance = this.bookTemplate.content.cloneNode(true);
+      bookInstance.querySelector('h4').textContent = book.title;
+      bookInstance.querySelector('p').textContent = book.author;
+      bookInstance.querySelector('.removeButton').addEventListener('click', () => {
+        removeBookFn()
+        this.render();
+      });
+      this.booksContainer.appendChild(bookInstance);
+    });
+  }
+}
+
+const bookList = new BookList();
+const booksListUI = new BooksListUI(bookList);
+
+document.getElementById('addBookButton').addEventListener('click', () => {
+  const bookTitle = document.getElementById('bookTitle').value;
+  const authorName = document.getElementById('bookAuthor').value;
+  const newBook = new Book(bookTitle, authorName);
+  bookList.addBook(newBook);
+  booksListUI.render();
+});
+
+booksListUI.render();
