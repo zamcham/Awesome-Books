de91362ee0f851493b0682acdf32d0c891635657
zamcham <cristian@zamcham.com>
2023-03-27 12:42:11 -0400

    Add book object module

---
+++ Modules/book.js
@@ -0,0 +1,8 @@
+// eslint-disable-next-line max-classes-per-file
+export default class Book {
+    constructor(title, author) {
+      this.title = title;
+      this.author = author;
+    }
+  }
+  
