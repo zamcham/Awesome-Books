787e3b69fa34cf4129b9bda5ebab553c21f96e89
zamcham <cristian@zamcham.com>
2023-03-27 12:42:01 -0400

    Refactor to use modular ES6

--- index.js
+++ index.js
@@ -1,61 +1,7 @@
-// #region Book Render/Remove Functions
-// eslint-disable-next-line max-classes-per-file
-class Book {
-  constructor(title, author) {
-    this.title = title;
-    this.author = author;
-  }
-}
-
-class BookList {
-  constructor() {
-    this.booksInfo = JSON.parse(localStorage.getItem('books') || '[]');
-  }
-
-  renderBooks(renderFn) {
-    this.booksInfo.forEach((book, index) => {
-      renderFn(book, () => {
-        this.removeBook(index);
-      });
-    });
-  }
-
-  removeBook(index) {
-    this.booksInfo.splice(index, 1);
-    this.updateStorage();
-  }
-
-  addBook(book) {
-    this.booksInfo.push(book);
-    this.updateStorage();
-  }
-
-  updateStorage() {
-    localStorage.setItem('books', JSON.stringify(this.booksInfo));
-  }
-}
-
-class BooksListUI {
-  constructor(bookList) {
-    this.bookList = bookList;
-    this.bookTemplate = document.getElementById('bookInfoTemplate');
-    this.booksContainer = document.querySelector('.booksContainer');
-  }
-
-  render() {
-    this.booksContainer.innerHTML = '';
-    this.bookList.renderBooks((book, removeBookFn) => {
-      const bookInstance = this.bookTemplate.content.cloneNode(true);
-      bookInstance.querySelector('h4').textContent = book.title;
-      bookInstance.querySelector('p').textContent = book.author;
-      bookInstance.querySelector('.removeButton').addEventListener('click', () => {
-        removeBookFn();
-        this.render();
-      });
-      this.booksContainer.appendChild(bookInstance);
-    });
-  }
-}
+import Book from '/Modules/book.js';
+import BookList from '/Modules/bookList.js';
+import BooksListUI from '/Modules/booksListUI.js';
+import setupMenu from './Modules/menu.js';
 
 const bookList = new BookList();
 const booksListUI = new BooksListUI(bookList);
@@ -76,52 +22,6 @@ document.getElementById('addBookButton').addEventListener('click', (event) => {
 });
 
 booksListUI.render();
-// #endregion
-// #region Hide and Show other pages
-const menu = document.getElementById('navLinks');
-const menuLinks = menu.children;
 
-for (let i = 0; i < menuLinks.length; i += 1) {
-  // We add an event listener to each menu link
-  menuLinks[i].addEventListener('click', () => {
-    // We get the text of the link (name) to use it as parameter
-    const textValue = menuLinks[i].textContent;
-    // we need to make the textValue without spaces and make it lowercase
-    const noSpacesString = textValue.replace(/\s+/g, '').toLowerCase();
-    const container = document.getElementById(noSpacesString);
-    // If the cliked is the one showing we do nothing
-    if (!container.classList.contains('hidden')) {
-      return;
-    }
-    // else, we hide it
-
-    container.classList.toggle('hidden');
-
-    // We get all the containers in the body
-    const allContainers = document.querySelectorAll('.contentSection');
-    // We only show the one that doesn't have 'hidden' class
-    allContainers.forEach((c) => {
-      if (c !== container && !c.classList.contains('hidden')) {
-        c.classList.toggle('hidden');
-      }
-    });
-  });
-}
-// #region
-// #region Update Date and Time
-function displayLiveDate() {
-  const liveDateElement = document.querySelector('.live-date');
-  const now = new Date();
-  const options = {
-    month: 'long',
-    day: 'numeric',
-    year: 'numeric',
-    hour: '2-digit',
-    minute: '2-digit',
-  };
-  const formattedDate = new Intl.DateTimeFormat('en-US', options).format(now);
-  liveDateElement.textContent = formattedDate;
-}
-displayLiveDate();
-setInterval(displayLiveDate, 30000);
-// #region
+// Call the setupMenu function to initialize the menu
+setupMenu();
